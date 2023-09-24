5ebcda1727749705674272577564be6bb99062ea
vishal <jakharvishalv@gmail.com>
2023-09-24 17:06:10 +0530

    env change

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const controller = require('./controller/herocontroller');
 const cors = require('cors');
-const PORT = 5000;
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 var app = express();
 app.use(cors());
